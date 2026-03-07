496dac73852de68079dbdc47ec6e439a79b3bcee
kiner00 <kinermercurio@gmail.com>
2026-03-07 10:53:14 +0800

    remove pulse

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,8 +10,6 @@ use App\Observers\LessonCompletionObserver;
 use App\Observers\PostObserver;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Pulse\Facades\Pulse;
-
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void {}
@@ -21,13 +19,5 @@ class AppServiceProvider extends ServiceProvider
         Post::observe(PostObserver::class);
         Comment::observe(CommentObserver::class);
         LessonCompletion::observe(LessonCompletionObserver::class);
-
-        Gate::define('viewPulse', fn ($user) => $user->is_super_admin);
-
-        Pulse::user(fn ($user) => [
-            'name'   => $user->name,
-            'extra'  => $user->email,
-            'avatar' => $user->avatar,
-        ]);
     }
 }
--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('subscriptions:renew')->dailyAt('08:00');
-Schedule::command('pulse:check')->everyMinute();
+// Schedule::command('pulse:check')->everyMinute();
