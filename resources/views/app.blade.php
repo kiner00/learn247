2d6cd6a20d1a39794fdee94df3923b5804937bb3
kiner00 <kinermercurio@gmail.com>
2026-03-13 22:31:42 +0800

    mvp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title inertia>{{ config('app.name', 'Learn247') }}</title>
-    <link rel="icon" type="image/png" href="/brand/ICON/CURZZO ICON MAIN.png" />
+    <link rel="icon" type="image/png" href="/brand/ICON WITH OUTLINE/CURZZO ICON G (with outline).png" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
