01b50bd8f5dba9f44e76d9813baa2c879466b676
kiner00 <kinermercurio@gmail.com>
2026-03-23 10:19:24 +0800

    ai landing page 3

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title inertia>{{ config('app.name', 'Learn247') }}</title>
     <link rel="icon" type="image/png" href="/brand/ICON/CURZZO LOGO WHIT BG ROUND.png" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
